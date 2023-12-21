6918747ac4df30cd9d62ddf3f39e9c3a7632a6ca
潇洒 <78096245+PizazzGY@users.noreply.github.com>
2023-12-21 22:12:06 +0800

    Add files via upload

--- 体育/JS/88看球.js
+++ 体育/JS/88看球.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'88看球',
     // host:'http://www.88kanqiu.cc',
-    host:'http://www.88kanqiu.one',
+    host:'http://www.88kanqiu.live',
     url:'/match/fyclass/live',
     searchUrl:'',
     searchable:0,
@@ -23,8 +23,7 @@ var rule = {
 	    "desc":";;;div.team-name:eq(0)&&Text;div.team-name:eq(1)&&Text",
 	    "content":"div.game-time&&Text",
 	    "tabs":"js:TABS=['88看球']",
-	    // "lists":"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+play_url+it.url});LISTS.push(d)});"
-	    "lists":"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)});"
+	    "lists":"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data.links;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)});"
 	},
     搜索:'',
 }
